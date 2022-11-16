ed4f51218f379f02e5772ecabbd057b78e096bd7
HbotondS <hbotond97@gmail.com>
2022-11-16 14:34:12 +0200

    fix authors script

--- db/create/createAuthors.js
+++ db/create/createAuthors.js
@@ -14,12 +14,20 @@ db.createCollection("Authors", {
                     description: "'peopleId' must be an ObjectId and is required"
                 },
                 genres: {
-                    bsonType: ["int"],
-                    description: "'genres' must be a string and is required"
+                    bsonType: "array",
+                    minItems: 1,
+                    description: "'genres' must be an array of objectId and is required",
+                    items: {
+                        bsonType: "objectId"
+                    }
                 },
                 knownFor: {
-                    bsonType: ["string"],
-                    description: "'knownFor' must be a string and is required"
+                    bsonType: "array",
+                    description: "'knownFor' must be an array string and is required",
+                    minItems: 1,
+                    items: {
+                        bsonType: "string"
+                    }
                 }
             }
         }
